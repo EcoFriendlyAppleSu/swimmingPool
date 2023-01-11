05311924d85d2b8483920934756c1513b7620b1f
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-01-11 14:55:57 +0900

    feat : address exception test

---
+++ src/test/java/swimming/pool/domain/swimmingpool/SwimmingPoolTest.java
@@ -0,0 +1,17 @@
+package swimming.pool.domain.swimmingpool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import swimming.pool.domain.swimmingpool.exception.AddressNotAllowException;
+
+class SwimmingPoolTest {
+
+  @Test
+  public void swimmingPoolAddressExceptionTest() throws Exception {
+    Assertions.assertThatThrownBy(() -> SwimmingPool.register("Lee", "영업중", "", ""))
+        .isInstanceOf(AddressNotAllowException.class);
+  }
+
+}
